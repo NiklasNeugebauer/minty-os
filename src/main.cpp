03977b0a97bfe6f2fca5ffc8d1d67a861cf99828
NiklasNeugebauer <niklas@neugebauer.dev>
2023-07-02 21:24:54 +0200

    set pebble face

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "MintyBase.h"
 #include "WatchFaces/HelloWorld.h"
 #include "WatchFaces/BasicTime.h"
+#include "WatchFaces/PebbleClock.h"
 
 //#include "./apps/_experiments/hello_world.h"
 
 MintyBase base;
 
 //HelloWorld watch_face;
-BasicTime watch_face;
-
+//BasicTime watch_face;
+PebbleClock watch_face;
 
 void setup () {
     base.wakeupRoutine();
