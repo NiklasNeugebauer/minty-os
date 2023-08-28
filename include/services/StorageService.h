db434cf0b398e1efbf710943202e9f6490d1ffa8
NiklasNeugebauer <niklas@neugebauer.dev>
2023-08-28 10:38:03 +0200

    add StorageService skeleton

---
+++ include/services/StorageService.h
@@ -0,0 +1,17 @@
+//
+// Created by niklas on 8/28/23.
+//
+
+#ifndef MINTY_OS_STORAGESERVICE_H
+#define MINTY_OS_STORAGESERVICE_H
+
+#include "Service.h"
+
+class StorageService : Service{
+public:
+    static void init();
+    static void update();
+};
+
+
+#endif //MINTY_OS_STORAGESERVICE_H
---
+++ src/services/StorageService.cpp
@@ -0,0 +1,13 @@
+//
+// Created by niklas on 8/28/23.
+//
+
+#include "services/StorageService.h"
+
+void StorageService::init() {
+
+}
+
+void StorageService::update() {
+
+}
