1b37aca26daabd462f54b1978931ad95a4eb7f57
NiklasNeugebauer <niklas@neugebauer.dev>
2023-08-28 18:55:46 +0200

    enable basic config storage

--- include/conf.h
+++ include/conf.h
@@ -1,6 +1,8 @@
 #ifndef MINTY_OS_CONF
 #define MINTY_OS_CONF
 
+#define MINTY_OS_VERSION_NUM "0.8"
+
 #define ARDUINO_WATCHY_V10
 #define NTP_SERVER "pool.ntp.org"
 #define HOLD_TICKS 10
--- src/services/StorageService.cpp
+++ src/services/StorageService.cpp
@@ -5,12 +5,35 @@
 #include "services/StorageService.h"
 #include <ArduinoNvs.h>
 #include "hal/hal.h"
-
+#include "SerialLogger.h"
 
 void StorageService::init() {
+    SERIAL_LOG_I("Initializing Non Volatile Storage");
     NVS.begin();
+    // check if storage has been initialized
+    String version_num = NVS.getString("OS_Version");
+    if (version_num.isEmpty()) {
+        // version number was not saved, assume storage is empty
+        SERIAL_LOG_D("No previous version info found.");
+    } else {
+        SERIAL_LOG_D("Previous Storage from version ", version_num);
+    }
+
+    NVS.setString("OS_Version", MINTY_OS_VERSION_NUM);
+    configuration current_config = Config::getGlobalConfig();
+    const configuration saved_config;
+    bool config_found = NVS.getBlob("Config", (uint8_t*)&saved_config, sizeof(configuration));
+    if (config_found) {
+        SERIAL_LOG_D("Previous configuration found! Restoring...");
+        memcpy(&current_config, &saved_config, sizeof(configuration));
+    } else {
+        SERIAL_LOG_D("No previous configuration found! Using default configuration...");
+        NVS.setBlob("Config", (uint8_t*)&current_config, sizeof(configuration));
+    }
+
+    NVS.commit();
 }
 
 void StorageService::update() {
-
+    NVS.begin();
 }
